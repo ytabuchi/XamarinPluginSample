27e35e1f0593e3e53b0fd20bd3aff0ef732cc24c
ytabuchi @ Y510P <ytabuchi.xlsoft@gmail.com>
2017-11-30 17:00:12 +0900

    Add plugin solution

---
+++ CalculateHash/CalculateHash/Plugin.CalculateHash.Abstractions/ICalculateHash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Plugin.CalculateHash.Abstractions
+{
+    /// <summary>
+    /// Interface for CalculateHash
+    /// </summary>
+    public interface ICalculateHash
+    {
+        string CalculateHash(string str, HashType type);
+    }
+
+    public enum HashType
+    {
+        md5,
+        sha1,
+        sha256,
+        sha512
+    }
+}
---
+++ CalculateHash/CalculateHash/Plugin.CalculateHash/CrossCalculateHash.cs
@@ -0,0 +1,43 @@
+﻿using Plugin.CalculateHash.Abstractions;
+using System;
+
+namespace Plugin.CalculateHash
+{
+  /// <summary>
+  /// Cross platform CalculateHash implemenations
+  /// </summary>
+  public class CrossCalculateHash
+  {
+    static Lazy<ICalculateHash> Implementation = new Lazy<ICalculateHash>(() => CreateCalculateHash(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+    /// <summary>
+    /// Current settings to use
+    /// </summary>
+    public static ICalculateHash Current
+    {
+      get
+      {
+        var ret = Implementation.Value;
+        if (ret == null)
+        {
+          throw NotImplementedInReferenceAssembly();
+        }
+        return ret;
+      }
+    }
+
+    static ICalculateHash CreateCalculateHash()
+    {
+#if PORTABLE
+        return null;
+#else
+        return new CalculateHashImplementation();
+#endif
+    }
+
+    internal static Exception NotImplementedInReferenceAssembly()
+    {
+      return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+    }
+  }
+}
